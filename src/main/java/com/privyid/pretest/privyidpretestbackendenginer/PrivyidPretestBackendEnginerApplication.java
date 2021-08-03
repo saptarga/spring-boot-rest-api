2736793ba0f44d53e69fa45e304e111ce8382653
saptarga <saptarga@gmail.com>
2021-08-03 14:43:11 +0700

    Initial Commit

---
+++ src/main/java/com/privyid/pretest/privyidpretestbackendenginer/PrivyidPretestBackendEnginerApplication.java
@@ -0,0 +1,13 @@
+package com.privyid.pretest.privyidpretestbackendenginer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivyidPretestBackendEnginerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivyidPretestBackendEnginerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/privyid/pretest/privyidpretestbackendenginer/PrivyidPretestBackendEnginerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privyid.pretest.privyidpretestbackendenginer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivyidPretestBackendEnginerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
